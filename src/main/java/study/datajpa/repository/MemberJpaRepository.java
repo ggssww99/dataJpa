c353212ea3d7c13812c286dd007158a8e882c7b9
ggssww99 <ggssww99@gmail.com>
2023-04-15 06:23:27 +0900

    메소드 이름으로 쿼리 생성

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -42,6 +42,10 @@ public class MemberJpaRepository {
         return em.find(Member.class, id);
     }
 
-
-
+    public List<Member> findByUsernameAgeGreaterThen(String username, int age){
+        return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+    }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -29,7 +29,6 @@ class MemberRepositoryTest {
         assertThat(findMember).isEqualTo(member);
     }
 
-
     @Test
     public void basicCRUD() {
         Member member1 = new Member("member1");
@@ -58,5 +57,19 @@ class MemberRepositoryTest {
 
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
     }
 }
